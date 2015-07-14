a35460a877fcd53ee548ad5e6e5a904e7772ca2b
unknown <am790d@TXCDTL122BS998H.ITServices.sbc.com>
2015-07-14 15:40:30 -0500

    Login setup

--- app/components/login/loginController.js
+++ app/components/login/loginController.js
@@ -8,7 +8,7 @@ angular.module("main").controller("loginController", function($scope, $rootScope
 	$scope.login = function() {
 		var UserClass = Parse.Object.extend("Users");
 		var query = new Parse.Query(UserClass);
-
+		
 		query.find({
 			success: function(results) {
 				for (var i=0; i<results.length; i++) {
@@ -16,6 +16,7 @@ angular.module("main").controller("loginController", function($scope, $rootScope
 						if (results[i].get("password") == $scope.loginInfo.password) {
 							$rootScope.user.username = $scope.loginInfo.username;
 							$rootScope.user.password = $scope.loginInfo.password;
+							$rootScope.user.loggedOn = true;
 							$rootScope.user.posts = results[i].get("posts");
 							return;
 						}
