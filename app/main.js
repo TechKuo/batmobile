9685841d14db0d37fd4f6067b237624aafc436dc
unknown <am790d@TXCDTL122BS998H.ITServices.sbc.com>
2015-07-15 08:59:46 -0500

    Post in the rootScope

--- app/main.js
+++ app/main.js
@@ -10,6 +10,10 @@ app.run(['$rootScope', '$location', function($rootScope, $location) {
 		loggedOn: false
 	};
 	
+	$rootScope.post = {
+		id: ""
+	};
+	
 	Parse.initialize("MOBxFrwSUvNUxvglhXSE92Dw2cRtPgo7vfDNjw1r", "nVZcYWW5VoSp3As1SJeOqlwfksRdlSJr2xGZjjv0");
 
     $rootScope.$on('$locationChangeStart', function(event, next, current) {
